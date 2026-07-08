f334e26474f225cd791b49a74f551fc8bdefecf2
sheharyar11223 <sheharyarmahar37@gmail.com>
2026-07-08 17:11:06 +0500

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,15 @@
+const num=[5,7,35,10,40,100,60,67,20,25,10,10]
+
+
+let tens = 0 
+
+for(let a=0;a<num.length;a++){
+    if(num[a]==10){
+       tens++
+}
+}
+
+
+
+
+console.log(` Divisible by Five are ${tens}`)
