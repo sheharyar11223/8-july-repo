60bbf4e174e5a0d0ed81dc88454cbe9a5573dd17
sheharyar11223 <sheharyarmahar37@gmail.com>
2026-07-08 16:53:17 +0500

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,16 @@
+const num=[5,7,35,10,40,100,60,67,20,25]
+
+let divisible_5 =0
+let others =0
+
+
+
+for(let a=0;a<num.length;a++){
+    if(num[a]%5===0){
+        divisible_5++
+}
+else{
+    others++
+}
+}
+console.log(` divisible by 5 are ${divisible_5}`)
