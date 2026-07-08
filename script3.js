e56e988e4a1ae1a2a8a9ffece583a3745108a1f7
sheharyar11223 <sheharyarmahar37@gmail.com>
2026-07-08 17:11:50 +0500

    Update num array and log counts of divisible and others
    Added additional elements to the num array and logged the count of numbers not divisible by 5.

--- script3.js
+++ script3.js
@@ -1,16 +1,21 @@
-const num=[5,7,35,10,40,100,60,67,20,25]
+const num=[5,7,35,10,40,100,60,67,20,25,10,10]
 
 let divisible_5 =0
 let others =0
 
 
-
 for(let a=0;a<num.length;a++){
     if(num[a]%5===0){
-        divisible_5++
+       divisible_5++
 }
 else{
     others++
 }
 }
+
+
+
+
 console.log(` divisible by 5 are ${divisible_5}`)
+console.log(` others are ${others}`)
+
